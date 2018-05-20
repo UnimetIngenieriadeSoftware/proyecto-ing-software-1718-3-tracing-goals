91b7611ea348850f3b5bfe94ff1201e06e668ad7
Wontser32 <fguzman53000@gmail.com>
2018-05-20 10:01:59 -0400

    cambio

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import androidApp from './android';
+import { YellowBox } from 'react-native';
+
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+
 
 AppRegistry.registerComponent('reactapp9', () => androidApp);
