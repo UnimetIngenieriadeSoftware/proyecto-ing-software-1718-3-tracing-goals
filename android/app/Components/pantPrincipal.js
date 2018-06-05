c829034084b0625f660bd8d222c2cd242fd331b9
Wontser32 <fguzman53000@gmail.com>
2018-06-05 15:39:24 -0400

    continuo mas tarde. esta quedando bien

--- android/app/Components/pantPrincipal.js
+++ android/app/Components/pantPrincipal.js
@@ -1188,10 +1188,15 @@ var tiempoDespuesNot;
       constructor(props){
         super(props);
         this.state = {
-          selectedHours1: 0,
-          selectedMinutes1: 0,
-          selectedHours2: 0,
-          selectedMinutes2: 0,
+          selectedInicioHours1: 0,
+          selectedInicioMinutes1: 0,
+          selectedFinHours1: 0,
+          selectedFinMinutes1: 0,
+
+          selectedInicioHours2: 0,
+          selectedInicioMinutes2: 0,
+          selectedFinHours2: 0,
+          selectedFinMinutes2: 0,
         };
       }
 
@@ -1207,11 +1212,15 @@ var tiempoDespuesNot;
           const usuariId = navigation.getParam('usuariId','dvalue');
           const vecesPorDia = navigation.getParam('vecesPorDia','dVValue');
 
-          var selectedHours1 = 0;
-          var selectedMinutes1 = 0;
+          var selectedInicioHours1 = 0;
+          var selectedInicioMinutes1 = 0;
+          var selectedFinHours1 = 0;
+          var selectedFinMinutes1 = 0;
 
-          var selectedHours2 = 0;
-          var selectedMinutes2 = 0;
+          var selectedInicioHours2 = 0;
+          var selectedInicioMinutes2 = 0;
+          var selectedFinHours2 = 0;
+          var selectedFinMinutes2 = 0;
 
           //si la rutina se repite 1 vez por dia que no se muestre el segundo timepicker
           //como hago? necesito una variable que esta true o false dependiendo del valor de vecesPorDia
@@ -1238,21 +1247,24 @@ var tiempoDespuesNot;
            
 
             <View>
+            <Text style={{fontSize: 25, color: '#525D3B', fontWeight: 'bold'}}> 1era </Text>
+              <Text style={{fontSize: 15, color: '#525D3B', fontWeight: 'bold'}}> Inicio </Text>
+
               <Text style={{fontSize: 25, color: '#525D3B',
                             fontWeight: 'bold'
-            }}> {this.state.selectedHours1}:{this.state.selectedMinutes1} </Text>
-              
+            }}> {this.state.selectedInicioHours1}:{this.state.selectedInicioMinutes1} </Text>
+
               <View style={{height: 150, width: width}}>
 
                 <Text style={{color: '#525D3B'}}> Hora </Text>
               
                       <Picker
-                        selectedValue={this.state.selectedHours1}        
+                        selectedValue={this.state.selectedInicioHours1}        
                         onValueChange={(itemValue, itemIndex) => {
                           this.setState({
-                            selectedHours1: itemValue
+                            selectedInicioHours1: itemValue
                           })
-                          selectedHours1=itemValue;
+                          selectedInicioHours1=itemValue;
                         } }
                       >
                         <Picker.Item label='0' value = '0' />
@@ -1284,12 +1296,12 @@ var tiempoDespuesNot;
 
                       <Text style={{color: '#525D3B'}}> Minuto </Text>
                       <Picker
-                        selectedValue={this.state.selectedMinutes1}        
+                        selectedValue={this.state.selectedInicioMinutes1}        
                         onValueChange={(itemValue, itemIndex) => {
                           this.setState({
-                            selectedMinutes1: itemValue
+                            selectedInicioMinutes1: itemValue
                           })
-                          selectedMinutes1=itemValue;
+                          selectedInicioMinutes1=itemValue;
                         } }
                       >
                         <Picker.Item label='0' value = '0' />
@@ -1357,6 +1369,139 @@ var tiempoDespuesNot;
               
               </View>
 
+                        <View>
+
+<Text style={{fontSize: 15, color: '#525D3B', fontWeight: 'bold'}}> Fin </Text>
+
+<Text style={{fontSize: 25, color: '#525D3B',
+              fontWeight: 'bold'
+}}> {this.state.selectedFinHours1}:{this.state.selectedFinMinutes1} </Text>
+
+<View style={{height: 150, width: width}}>
+
+  <Text style={{color: '#525D3B'}}> Hora </Text>
+
+        <Picker
+          selectedValue={this.state.selectedFinHours1}        
+          onValueChange={(itemValue, itemIndex) => {
+            this.setState({
+              selectedFinHours1: itemValue
+            })
+            selectedFinHours1=itemValue;
+          } }
+        >
+          <Picker.Item label='0' value = '0' />
+          <Picker.Item label='1' value = '1' />
+          <Picker.Item label='2' value = '2' />
+          <Picker.Item label='3' value = '3' />
+          <Picker.Item label='4' value = '4' />
+          <Picker.Item label='5' value = '5' />
+          <Picker.Item label='6' value = '6' />
+          <Picker.Item label='7' value = '7' />
+          <Picker.Item label='8' value = '8' />
+          <Picker.Item label='9' value = '9' />
+          <Picker.Item label='10' value = '10' />
+          <Picker.Item label='11' value = '11' />
+          <Picker.Item label='12' value = '12' />
+          <Picker.Item label='13' value = '13' />
+          <Picker.Item label='14' value = '14' />
+          <Picker.Item label='15' value = '15' />
+          <Picker.Item label='16' value = '16' />
+          <Picker.Item label='17' value = '17' />
+          <Picker.Item label='18' value = '18' />
+          <Picker.Item label='19' value = '19' />
+          <Picker.Item label='20' value = '20' />
+          <Picker.Item label='21' value = '21' />
+          <Picker.Item label='22' value = '22' />
+          <Picker.Item label='23' value = '23' />
+        </Picker>
+
+
+        <Text style={{color: '#525D3B'}}> Minuto </Text>
+        <Picker
+          selectedValue={this.state.selectedMinutes1}        
+          onValueChange={(itemValue, itemIndex) => {
+            this.setState({
+              selectedFinMinutes1: itemValue
+            })
+            selectedFinMinutes1=itemValue;
+          } }
+        >
+          <Picker.Item label='0' value = '0' />
+          <Picker.Item label='1' value = '1' />
+          <Picker.Item label='2' value = '2' />
+          <Picker.Item label='3' value = '3' />
+          <Picker.Item label='4' value = '4' />
+          <Picker.Item label='5' value = '5' />
+          <Picker.Item label='6' value = '6' />
+          <Picker.Item label='7' value = '7' />
+          <Picker.Item label='8' value = '8' />
+          <Picker.Item label='9' value = '9' />
+          <Picker.Item label='10' value = '10' />
+          <Picker.Item label='11' value = '11' />
+          <Picker.Item label='12' value = '12' />
+          <Picker.Item label='13' value = '13' />
+          <Picker.Item label='14' value = '14' />
+          <Picker.Item label='15' value = '15' />
+          <Picker.Item label='16' value = '16' />
+          <Picker.Item label='17' value = '17' />
+          <Picker.Item label='18' value = '18' />
+          <Picker.Item label='19' value = '19' />
+          <Picker.Item label='20' value = '20' />
+          <Picker.Item label='21' value = '21' />
+          <Picker.Item label='22' value = '22' />
+          <Picker.Item label='23' value = '23' />
+          <Picker.Item label='24' value = '24' />
+          <Picker.Item label='25' value = '25' />
+          <Picker.Item label='26' value = '26' />
+          <Picker.Item label='27' value = '27' />
+          <Picker.Item label='28' value = '28' />
+          <Picker.Item label='29' value = '29' />
+          <Picker.Item label='30' value = '30' />
+          <Picker.Item label='31' value = '31' />
+          <Picker.Item label='32' value = '32' />
+          <Picker.Item label='33' value = '33' />
+          <Picker.Item label='34' value = '34' />
+          <Picker.Item label='35' value = '35' />
+          <Picker.Item label='36' value = '36' />
+          <Picker.Item label='37' value = '37' />
+          <Picker.Item label='38' value = '38' />
+          <Picker.Item label='39' value = '39' />
+          <Picker.Item label='40' value = '40' />
+          <Picker.Item label='41' value = '41' />
+          <Picker.Item label='42' value = '42' />
+          <Picker.Item label='43' value = '43' />
+          <Picker.Item label='44' value = '44' />
+          <Picker.Item label='45' value = '45' />
+          <Picker.Item label='46' value = '46' />
+          <Picker.Item label='47' value = '47' />
+          <Picker.Item label='48' value = '48' />
+          <Picker.Item label='49' value = '49' />
+          <Picker.Item label='50' value = '50' />
+          <Picker.Item label='51' value = '51' />
+          <Picker.Item label='52' value = '52' />
+          <Picker.Item label='53' value = '53' />
+          <Picker.Item label='54' value = '54' />
+          <Picker.Item label='55' value = '55' />
+          <Picker.Item label='56' value = '56' />
+          <Picker.Item label='57' value = '57' />
+          <Picker.Item label='58' value = '58' />
+          <Picker.Item label='59' value = '59' />
+        </Picker>
+        </View>
+
+</View>
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -1369,18 +1514,23 @@ var tiempoDespuesNot;
                     showSecond ? 
                   <View style={{height: 200, width: 300}}>
 
+                  <Text style={{fontSize: 25, color: '#525D3B', fontWeight: 'bold'}}> 2da </Text>
+
+
+                    <Text style={{fontSize: 15, color: '#525D3B', fontWeight: 'bold'}}> Inicio </Text>
+
                 <Text style={{fontSize: 25, color: '#525D3B',
                             fontWeight: 'bold'
-            }}> {this.state.selectedHours2}:{this.state.selectedMinutes2} </Text>
+            }}> {this.state.selectedInicioHours2}:{this.state.selectedInicioMinutes2} </Text>
 
               <Text style={{color: '#525D3B'}}> Hora </Text>
               <Picker
-                selectedValue={this.state.selectedHours2}        
+                selectedValue={this.state.selectedInicioHours2}        
                 onValueChange={(itemValue, itemIndex) => {
                   this.setState({
-                    selectedHours2: itemValue
+                    selectedInicioHours2: itemValue
                   })
-                  selectedHours2=itemValue;
+                  selectedInicioHours2=itemValue;
                 } }
               >
                 <Picker.Item label='0' value = '0' />
@@ -1412,12 +1562,12 @@ var tiempoDespuesNot;
 
               <Text style={{color: '#525D3B'}}> Minuto </Text>
               <Picker
-                selectedValue={this.state.selectedMinutes2}        
+                selectedValue={this.state.selectedInicioMinutes2}        
                 onValueChange={(itemValue, itemIndex) => {
                   this.setState({
-                    selectedMinutes2: itemValue
+                    selectedInicioMinutes2: itemValue
                   })
-                  selectedMinutes2=itemValue;
+                  selectedInicioMinutes2=itemValue;
                 } }
               >
                 <Picker.Item label='0' value = '0' />
@@ -1489,6 +1639,38 @@ var tiempoDespuesNot;
             </View>
 
 
+{/*
+            <Grid>
+                <Col style={{ backgroundColor: '#f2f4fc', height: 140, width: 119}}></Col>
+                    <Button 
+                              onPress= {() => {
+                                repPorDia = this.state.repeticionesPorDia;
+                                this.props.navigation.navigate('crearRutina3',
+                                {
+                                nombreRut: nombreRut, 
+                                numeroPriorRut: numeroPriorRut,
+                                nameUser: nameUser,
+                                metaId: metaId,
+                                tiempoAntesNot: tiempoAntesNot,
+                                tiempoDespuesNot: tiempoDespuesNot,
+                                usuariId: usuariId,
+                                vecesPorDia: repPorDia,
+                                  //mandar las 2 horas elegidas
+                          
+                                } 
+                              )
+                              } }
+                              style={{fontSize: 15, color: '#f2f4fc'}}
+                              containerStyle={{padding: 10, height: 45, overflow: 'hidden', borderRadius: 15, backgroundColor: '#525D3B'}}
+                    >
+                              Continuar {space}
+                              <TrophyIcon name='trophy' size={15} color= 'gold'>
+                              </TrophyIcon>
+                    </Button>
+                    <Col style={{ backgroundColor: '#f2f4fc', height: 140, width: 119}}></Col>
+          </Grid>
+
+                            */}
 
        </Form>
      </Content>         
